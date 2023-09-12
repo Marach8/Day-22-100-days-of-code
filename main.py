c12b90156c9161cf6499cadd5d804b1404fc021d
Marach8 <nnannamarach4@gmail.com>
2023-09-12 11:43:50 +0000

    Learned to use imports. Used the random.randint()

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ while True:
     continue
   elif ask > y:
     print()
-    print('\033[31mYour guess is too high! try again.\033[0m')
+    print('\033[31mYour guess is too high! try again!\033[0m')
     x += 1
     continue
   elif ask < 0:
